3515ff7d6636713e7fe99349f434bac5df947a7d
ipek <ipekcivicakan@gmail.com>
2019-01-08 10:55:28 -0800

    usePropy is a actual reuse

--- src/components/ResourceList.js
+++ src/components/ResourceList.js
@@ -1,31 +1,23 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
+const usePropy = (propy) => {
+    const [resources, setResources] = useState([]);
+    useEffect(
+        () => {
+            (async (propy) => {
+                const response = await axios.get(`https://jsonplaceholder.typicode.com/${propy}`)
+
+                setResources(response.data);
+            })(propy)
+        }, [propy])
+
+    return resources;
+}
 
 const ResourceList = ({ propy }) => {
-    const [resources, setResources] = useState([]);
 
-
-    //state = { propy: [] };
-
-    const fetchResource = async (propy) => {
-        const response = await axios.get(`https://jsonplaceholder.typicode.com/${propy}`)
-
-        setResources(response.data);
-    }
-
-    useEffect(() => {
-        fetchResource(propy)
-    }, [propy])
-
-
-    // async componentDidUpdate(prevProps) {
-    //     if (prevProps.propy !== this.props.propy) {
-    //         const response = await axios.get(`https://jsonplaceholder.typicode.com/${this.props.propy}`)
-    //         this.setState({ propy: response.data })
-    //     }
-    // }
-
+    const resources = usePropy(propy);
 
     return (
         <div>{resources.map(record => (
